aa808b940bd464e61d1ed061bd0e493c016447c8
Steve McGrath <steve@chigeek.com>
2019-11-01 16:12:35 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = 'Please refer to https://restfly.readthedocs.io'
 
 setup(
     name='restfly',
-    version='1.0.3',
+    version='1.1',
     description='A library to make API wrappers creation easier',
     author='Steve McGrath',
     long_description=long_description,
