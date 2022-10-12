aad0c86cde954a97905181095c5d9068ad15ac0e
Steven McGrath <steve@chigeek.com>
2022-10-12 13:55:58 -0500

    Updated documentation and repointed links to new repo location (#9)

--- README.rst
+++ README.rst
@@ -3,14 +3,16 @@ RESTfly
 
 .. image:: docs/_static/logo.png
 
-.. image:: https://github.com/SteveMcGrath/restfly/actions/workflows/pipeline.yml/badge.svg
-   :target: https://github.com/SteveMcGrath/restfly/actions/workflows/pipeline.yml
+.. image:: https://github.com/librestfly/restfly/actions/workflows/testing.yml/badge.svg
+   :target: https://github.com/librestfly/restfly/actions/workflows/testing.yml
+.. image:: https://sonarcloud.io/api/project_badges/measure?project=librestfly_restfly&metric=alert_status
+   :target: https://sonarcloud.io/summary/overall?id=librestfly_restfly
 .. image:: https://img.shields.io/pypi/v/restfly.svg
    :target: https://pypi.org/project/restfly/
 .. image:: https://img.shields.io/pypi/pyversions/restfly.svg
-   :target: https://pypi.org/project/restfly/
 .. image:: https://img.shields.io/pypi/dm/restfly.svg
-.. image:: https://img.shields.io/github/license/stevemcgrath/restfly.svg
+.. image:: https://img.shields.io/github/license/librestfly/restfly.svg
+   :target: https://github.com/librestfly/restfly/blob/master/LICENSE
 
 A simple framework for building RESTful API libraries.  for more information,
 please refer to the documentation at https://restfly.readthedocs.io
--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+'''
+RESTfly Package
+'''
 from setuptools import setup, find_packages
 
-with open('restfly/version.py', 'r') as vfile:
-    exec(vfile.read())
+with open('restfly/version.py', 'r') as vfile:  # noqa PLW1514
+    exec(vfile.read())  # noqa PLW0122
 
 long_description = '''
 RESTfly: Simplifying API Libraries
@@ -10,14 +13,16 @@ RESTfly: Simplifying API Libraries
 **RESTfly** (pronounced restfully) is a framework for building libraries to
 easily interact with RESTful APIs.
 
-.. image:: https://travis-ci.org/SteveMcGrath/restfly.svg?branch=master
-   :target: https://travis-ci.org/SteveMcGrath/restfly
+.. image:: https://travis-ci.org/librestfly/restfly.svg?branch=master
+   :target: https://travis-ci.org/librestfly/restfly
 .. image:: https://img.shields.io/pypi/v/restfly.svg
    :target: https://pypi.org/project/restfly/
 .. image:: https://img.shields.io/pypi/pyversions/restfly.svg
 .. image:: https://img.shields.io/pypi/dm/restfly.svg
-.. image:: https://img.shields.io/github/license/stevemcgrath/restfly.svg
-   :target: https://github.com/SteveMcGrath/restfly/blob/master/LICENSE
+.. image:: https://img.shields.io/github/license/librestfly/restfly.svg
+   :target: https://github.com/librestfly/restfly/blob/master/LICENSE
+.. image:: https://sonarcloud.io/api/project_badges/measure?project=librestfly_restfly&metric=alert_status
+   :target: https://sonarcloud.io/summary/overall?id=librestfly_restfly
 
 
 .. image:: https://restfly.readthedocs.io/en/latest/_static/logo.png
@@ -26,19 +31,19 @@ RESTfly is a simple library designed to provide the scaffolding to make API
 interaction libraries for just about any RESTful API.  With an emphasis on
 simplicity and readability of the resulting library code thats written, the
 idea here is provide something that is not only useful for folks to use, but
-can also serve as a reference implimentation of the given APIs.
+can also serve as a reference implementation of the given APIs.
 
 Please refer to the full documentation at https://restfly.readthedocs.io.
-'''
+'''  # noqa E501
 
 setup(
     name='restfly',
-    version=VERSION,  # noqa: F821
+    version=VERSION,  # noqa: F821,PLE0602
     description='A library to make API wrappers creation easier',
     author='Steve McGrath',
     long_description=long_description,
     author_email='steve@chigeek.com',
-    url='https://github.com/stevemcgrath/restfly',
+    url='https://github.com/librestfly/restfly',
     license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -53,7 +58,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
-    keywords='tenable tenable_io securitycenter containersecurity',
+    keywords='rest api library',
     packages=find_packages(exclude=['docs', 'tests']),
     install_requires=[
         'requests>=2.26.0',
